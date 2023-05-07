20e0e96e5d85c3d095905832c1bdf72b8261a801
Artser <artserlisa@mail.ru>
2023-05-07 20:21:34 +0300

    нашла работающий

--- Encapsulation2/src/Main.java
+++ Encapsulation2/src/Main.java
@@ -3,25 +3,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+         /*Код для Elevator
         Elevator elevator = new Elevator(-3, 26);
 
         while(true) {
             System.out.print("Введите номер этажа: ");
             int floor = new Scanner(System.in).nextInt();
             elevator.move(floor);
-        }
-        Dimensions dimensions = new Dimensions(3,2,1);
+        }*/
 
-        CargoInformation cargoInfo = new CargoInformation(dimensions, 40, "Ростов-на-Дону, Ленина пр.101", true, "340038", false, dimensions);
+        Dimensions dimensions1 = new Dimensions(3,2,1);
+        dimensions1 = dimensions1.setHeight(5);
 
-        System.out.println("Информация о грузе :" + "\n"
-                + "Вес" + cargoInfo.getWeight() + "\n"
-                + "Адрес доставки" + cargoInfo.getDeliveryAdress() + "\n"
-                + "Можно переворачивать" + cargoInfo.isFlip() + "\n"
-                + "Регистрационный номер" + cargoInfo.getRegistrationNumber() + "\n"
-                + "Хрупкость" + cargoInfo.isFlip() + "\n"
-                + "Обьем груза" + cargoInfo.getDimensions());
+        CargoInformation cargoOrders1 = new CargoInformation(dimensions1,5,"г.Спб, ул.Садовая, д.12",
+                true,"NA985623",false);
 
+
+        System.out.println(cargoOrders1);
 
     }
 }
